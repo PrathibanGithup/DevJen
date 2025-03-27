e71904bc609f2dff3afb133cb1affe267d144c23
PrathibanGithup <Prathipalani19@gmail.com>
2025-03-27 09:42:22 +0530

    gevjen Message

---
+++ src/main/java/com/dev/DevJenApplication.java
@@ -0,0 +1,13 @@
+package com.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevJenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevJenApplication.class, args);
+	}
+
+}
